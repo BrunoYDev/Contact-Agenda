980057a0e198583c93ba07db75c5818fb20363ec
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2023-11-29 20:25:31 -0300

    feat: add pdf report route

--- backend/src/modules/clients/clients.controller.ts
+++ backend/src/modules/clients/clients.controller.ts
@@ -8,13 +8,15 @@ import {
   Delete,
   HttpCode,
   UseGuards,
-  Request
+  Request,
+  Res
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ApiTags,ApiBearerAuth } from '@nestjs/swagger/dist';
+
 
 @ApiTags('clients')
 @Controller('clients')
@@ -24,6 +26,21 @@ export class ClientsController {
   @Post()
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientsService.create(createClientDto);
+  }
+
+  @Get('report')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  async makePdfReport(@Res() res, @Request() req): Promise<void> {
+
+    const buffer = await this.clientsService.makePdfReport(req.user.id);
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=example.pdf',
+      'Content-Length': buffer.length,
+    })
+
+    res.end(buffer);
   }
 
   @Get()
--- backend/src/modules/clients/clients.service.ts
+++ backend/src/modules/clients/clients.service.ts
@@ -2,13 +2,15 @@ import {
   Injectable,
   ConflictException,
   NotFoundException,
-  UnauthorizedException
+  UnauthorizedException,
 } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { PrismaService } from 'src/database/prisma.service';
 import { Client } from './entities/client.entity';
 import { plainToInstance } from 'class-transformer';
+import fs from 'node:fs';
+const PDFDocument = require('pdfkit-table');
 
 @Injectable()
 export class ClientsService {
@@ -60,8 +62,8 @@ export class ClientsService {
     const client = await this.prisma.client.findUnique({
       where: { id },
     });
-    if(id !== loggedId){
-      throw new UnauthorizedException('Only the account owner can update.')
+    if (id !== loggedId) {
+      throw new UnauthorizedException('Only the account owner can update.');
     }
     if (!client) {
       throw new NotFoundException('Client not found.');
@@ -73,12 +75,178 @@ export class ClientsService {
     return plainToInstance(Client, updatedClient);
   }
 
+  async makePdfReport(id: string): Promise<Buffer> {
+    const client = await this.prisma.client.findUnique({
+      where: { id },
+    });
+    if (!client) {
+      throw new NotFoundException('Client not found.');
+    }
+    const clientContacts = await this.prisma.contact.findMany({
+      where: { clientId: id },
+    });
+    const pdfBuffer: Buffer = await new Promise((resolve) => {
+      const doc = new PDFDocument({
+        size: 'B4',
+        bufferPages: true,
+      });
+
+      const newClientContacts = [];
+      for (let i = 0; i < clientContacts.length; i++) {
+        const newContactRegisterDate =
+          clientContacts[i].registerDate.toISOString();
+        const newContact = {
+          ...clientContacts[i],
+          registerDate: newContactRegisterDate,
+        };
+        newClientContacts.push(newContact);
+      }
+
+      const pdfText = `Contatos do usuário ${client.fullname}`;
+      const contacts = {
+        title: pdfText,
+        valign: 'center',
+        headers: [
+          {
+            label: 'Nome completo do contato',
+            property: 'fullname',
+            width: 160,
+            align: 'center',
+            valign: 'center',
+          },
+          {
+            label: 'Email',
+            property: 'email',
+            width: 140,
+            align: 'center',
+            valign: 'center',
+            headerVAlign: "center",
+          },
+          {
+            label: 'Numero de telefone',
+            property: 'cellphone',
+            width: 130,
+            align: 'center',
+            valign: 'center',
+          },
+          {
+            label: 'Data de registro',
+            property: 'registerDate',
+            width: 150,
+            align: 'center',
+            valign: 'center',
+          },
+        ],
+        datas: newClientContacts,
+      };
+      const clientInfo = {
+        valign: 'center',
+        headers: [
+          {
+            label: 'Email',
+            property: 'email',
+            width: 215,
+            align: 'center',
+            valign: 'center',
+          },
+          {
+            label: 'Numero de telefone',
+            property: 'cellphone',
+            width: 200,
+            align: 'center',
+            valign: 'center',
+          },
+          {
+            label: 'Data de registro',
+            property: 'registerDate',
+            width: 145,
+            align: 'center',
+            valign: 'center',
+          },
+        ],
+        datas: [
+          {
+            email: client.email,
+            cellphone: client.cellphone,
+            registerDate: client.registerDate.toISOString(),
+          },
+        ],
+      };
+
+      doc.fontSize(24);
+      doc.fillColor('black').text(`Dados do usuário ${client.fullname}`, {
+        align: 'center',
+        stroke: 20,
+      });
+      doc.moveDown(1);
+      doc.table(clientInfo, {
+        minRowHeight: 40,
+        padding: 10,
+        align: 'center',
+        prepareHeader: () => doc.font('Helvetica-Bold').fontSize(12),
+        prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+          doc.font('Helvetica').fontSize(16).fillColor('black');
+          const { x, y, width, height } = rectCell;
+          if (indexColumn === 0) {
+            doc
+              .lineWidth(0.5)
+              .moveTo(x, y)
+              .lineTo(x, y + height)
+              .stroke();
+          }
+
+          doc
+            .lineWidth(0.5)
+            .moveTo(x + width, y)
+            .lineTo(x + width, y + height)
+            .stroke();
+          indexColumn === 0 && doc.addBackground(rectRow, 'green', 0.15);
+        },
+      });
+      doc.moveDown(2);
+      doc.table(contacts, {
+        minRowHeight: 40,
+        padding: 10,
+        align: 'center',
+        prepareHeader: () => doc.font('Helvetica-Bold').fontSize(14),
+        prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+          doc.font('Helvetica').fontSize(12);
+
+          const { x, y, width, height } = rectCell;
+          if (indexColumn === 0) {
+            doc
+              .lineWidth(0.5)
+              .moveTo(x, y)
+              .lineTo(x, y + height)
+              .stroke();
+          }
+
+          doc
+            .lineWidth(0.5)
+            .moveTo(x + width, y)
+            .lineTo(x + width, y + height)
+            .stroke();
+          indexColumn === 0 && doc.addBackground(rectRow, 'blue', 0.15);
+        },
+      });
+
+      const buffer = [];
+      doc.on('data', buffer.push.bind(buffer));
+      doc.on('end', () => {
+        const data = Buffer.concat(buffer);
+        resolve(data);
+      });
+      doc.end();
+    });
+    return pdfBuffer;
+  }
+
   async remove(id: string, loggedId: string) {
     const client = await this.prisma.client.findUnique({
       where: { id },
     });
-    if(id !== loggedId){
-      throw new UnauthorizedException('Only the account owner can delete.')
+    if (id !== loggedId) {
+      throw new UnauthorizedException('Only the account owner can delete.');
     }
     if (!client) {
       throw new NotFoundException('Client not found.');

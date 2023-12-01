08d93879f0c3af6c883a757e80f093cc68157c7c
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2023-12-01 10:55:04 -0300

    Feat: add frontend and Fix: cors bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cors from 'cors';
+const cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
